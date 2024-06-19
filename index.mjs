f7ac86f06d4b0a83923fb1fc0b053a10fc41d7e1
Kristofer <kristofer@andes.is>
2024-06-19 15:52:44 +0000

    Updates

---
+++ index.mjs
@@ -0,0 +1,15 @@
+import { core } from "@actions/core";
+import { process } from "node:process";
+
+async function run() {
+  try {
+    core.exportVariable(
+      "BRANCH_NAME",
+      process.env.GITHUB_REF.split("/").slice(2).join("/"),
+    );
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
